3ee9f8229ae3b8c2b0c6461ed171794153397853
ikhoda <124679524+ikhoda@users.noreply.github.com>
2023-05-01 12:47:50 +0200

    Add files via upload

---
+++ HW250423FE/script.js
@@ -0,0 +1,29 @@
+// for(let i = 0; i <= 10; i++) {
+//     if(i % 2 === 0) {
+//         console.log(i)
+//     }
+// }
+
+// for(let i = 55; i >= 20; i--)  {
+//     console.log(i)
+// }
+// const numbers = [3, 5, 11, 2, 8, 1, 6];
+// const numbers_squared = []
+// for(let i = 0; i < numbers.length; i++) {
+//     const newNumber = numbers[i] ** 2;
+//     numbers_squared.push(newNumber)
+// }
+// console.log(numbers_squared)
+
+// const last_elem = []
+// const newElement = numbers[6]
+// last_elem.push(newElement)
+// console.log(last_elem)
+
+const user = {
+    first_name: 'Ivan', 
+    last_name: 'Ivanov', 
+    age: 20, 
+    salary: 500
+    }
+console.log(`User's name is ${user.first_name} ${user.last_name}, He is ${user.age} years old`)
