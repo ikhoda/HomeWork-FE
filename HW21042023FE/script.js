73ab8e2df8c255d1c1a973a25da5a5eeff0132e7
ikhoda <124679524+ikhoda@users.noreply.github.com>
2023-05-01 11:52:27 +0200

    Add files via upload

---
+++ HW21042023FE/script.js
@@ -0,0 +1,28 @@
+// const number = prompt("Введите число")
+// const sales = number * 0.1
+// console.log(sales)
+
+// const number1 = +prompt("Введите первое число")
+// const number2 = +prompt("Введите второе число")
+// if(number1 > number2) {
+//     console.log(number2)
+// } else if(number1 < number2) {
+//     console.log(number1)
+// }
+
+// const number = +prompt("Введите число")
+// if(number < 100) {
+//      console.log("Число меньше 100 ")
+//  } else if(number > 100) {
+//      console.log("Число больше 100")
+//  } else {
+//      console.log("Число равно 100")
+//  }
+
+// const name = +prompt("Ваше имя");
+// const age = +prompt("Ваш возраст")
+// if(age > 18) {
+//     console.log("Hello, " + name)
+// } else {
+//     console.log("Hi, " + name)
+// }
